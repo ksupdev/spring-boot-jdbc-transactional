3288a217b182e04aabbee4110c5216546a75c973
ksupdev <karoons.updev@gmail.com>
2020-10-17 12:59:18 +0700

    setup spring boot project

---
+++ src/main/java/com/updev/springbootjdbctransactional/SpringBootJdbcTransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.updev.springbootjdbctransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcTransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcTransactionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/updev/springbootjdbctransactional/SpringBootJdbcTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.updev.springbootjdbctransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
